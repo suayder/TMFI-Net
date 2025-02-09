fixing numerical instability in cc los

loss.py
@@ -16,7 +16,7 @@ def kldiv(s_map, gt):
     w = s_map.size(1)
     h = s_map.size(2)
 
-    eps = 2.2204e-16
+    eps = 2.2204e-8
 
     sum_s_map = torch.sum(s_map.view(batch_size, -1), 1)
     expand_s_map = sum_s_map.view(batch_size, 1, 1).expand(batch_size, w, h)
@@ -28,8 +28,8 @@ def kldiv(s_map, gt):
     
     assert expand_gt.size() == gt.size()
 
-    s_map = s_map/((expand_s_map+eps)*1.0)
-    gt = gt / ((expand_gt+eps)*1.0)
+    s_map = s_map/(expand_s_map+eps)
+    gt = gt / (expand_gt+eps)
 
     s_map = s_map.view(batch_size, -1)
     gt = gt.view(batch_size, -1)
@@ -38,6 +38,7 @@ def kldiv(s_map, gt):
     result = gt * torch.log(eps + gt/(s_map + eps))
     # print('losses', torch.log(eps + gt/(s_map + eps)))
     # print('loss', gt * torch.log(eps + gt/(s_map + eps)))
+    # print('kldiv', torch.mean(torch.sum(result, 1)), f'is nan {torch.isnan(s_map).any()}')
     return torch.mean(torch.sum(result, 1))
 
 
@@ -51,7 +52,7 @@ def normalize_map(s_map):
     min_s_map = torch.min(s_map.view(batch_size, -1), 1)[0].view(batch_size, 1, 1).expand(batch_size, w, h)
     max_s_map = torch.max(s_map.view(batch_size, -1), 1)[0].view(batch_size, 1, 1).expand(batch_size, w, h)
 
-    norm_s_map = (s_map - min_s_map)/((max_s_map-min_s_map*1.0)+eps)
+    norm_s_map = (s_map - min_s_map)/(max_s_map-min_s_map+eps)
     return norm_s_map
 
 def similarity(s_map, gt):
@@ -84,7 +85,7 @@ def similarity(s_map, gt):
     return torch.mean(torch.sum(torch.min(s_map, gt), 1))
 
 def cc(s_map, gt):
-    eps = 2.2204e-16
+    eps = 2.2204e-8
 
     assert s_map.size() == gt.size()
     batch_size = s_map.size(0)
@@ -104,7 +105,7 @@ def cc(s_map, gt):
     aa = torch.sum((s_map * s_map).view(batch_size, -1), 1)
     bb = torch.sum((gt * gt).view(batch_size, -1), 1)
 
-    return torch.mean(ab / (torch.sqrt(aa*bb)+eps))
+    return torch.mean(ab / torch.sqrt(torch.clamp(aa*bb, min=eps)))
 
 def nss(s_map, gt):
     if s_map.size() != gt.size():

train.py
@@ -35,11 +35,11 @@ parser.add_argument('--nss_coeff', default=-1.0, type=float)
 parser.add_argument('--l1_coeff', default=1.0, type=float)
 
 parser.add_argument('--no_epochs', default=300, type=int)
-parser.add_argument('--lr', default=1e-6, type=float)
-parser.add_argument('--batch_size', default=4, type=int)
+parser.add_argument('--lr', default=1e-4, type=float)
+parser.add_argument('--batch_size', default=2, type=int)
 parser.add_argument('--log_interval', default=10, type=int)
 parser.add_argument('--no_workers', default=4, type=int)
-parser.add_argument('--model_val_path', default="./saved_models/", type=str)
+parser.add_argument('--model_val_path', default="/scratch/suayder/jbcs_experiments/", type=str)
 parser.add_argument('--clip_size', default=32, type=int)
 
 parser.add_argument('--train_path_data', default="/scratch/suayder/urbanaccess_dhf1/train", type=str)
@@ -72,11 +72,13 @@ if torch.cuda.device_count() > 1:
     model = nn.DataParallel(model)
 model.to(device)
 
-params = model.parameters()
-optimizer = torch.optim.Adam(params, lr=args.lr)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
+def is_finite(model):
+    return any(torch.isfinite(p).all() for p in model.parameters())
 
 def train(model, optimizer, loader, epoch, device, args, writer):
+    # torch.autograd.set_detect_anomaly(True)
     model.train()
 
     total_loss = AverageMeter()
@@ -88,16 +90,28 @@ def train(model, optimizer, loader, epoch, device, args, writer):
         img_clips = img_clips.to(device)
         img_clips = img_clips.permute((0, 2, 1, 3, 4))
         gt_sal = gt_sal.to(device)
+        assert torch.isfinite(img_clips).all() and torch.isfinite(gt_sal).all(), 'non finite value at the input'
 
-        optimizer.zero_grad()
         z0 = model(img_clips)
-
+    
         assert z0.size() == gt_sal.size()
         loss = loss_func(z0, gt_sal, args)
+        assert torch.isfinite(loss).all(), 'non finite value returned at loss function'
 
+        optimizer.zero_grad()
         loss.backward()
-        optimizer.step()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
 
+        for name, param in model.named_parameters():
+            if param.grad is not None:
+                if torch.isnan(param.grad).any():
+                    print(f"❌ NaN detected in gradient of {name}!")
+                    exit()  # Stop execution if NaN is found
+                if torch.isinf(param.grad).any():
+                    print(f"⚠️ Inf detected in gradient of {name}!")
+                    exit()
+
+        optimizer.step()
         total_loss.update(loss.item())
         cur_loss.update(loss.item())
 
@@ -168,4 +182,5 @@ for epoch in range(0, args.no_epochs):
                     torch.save(model.module.state_dict(), args.model_val_path + 'best_VSTNet.pth'.format(epoch))
                 else:
                     torch.save(model.state_dict(), args.model_val_path + 'best_VSTNet.pth'.format(epoch))
+                print('model saved at:', args.model_val_path + 'best_VSTNet.pth')
 writer.close()

utils1.py
@@ -17,13 +17,12 @@ def get_loss(pred_map, gt, args):
         loss += args.l1_coeff * criterion(pred_map, gt)
     if args.sim:
         loss += args.sim_coeff * similarity(pred_map, gt)
-
+    
     return loss
 
 
 def loss_func(pred_map, gt, args):
     loss = torch.FloatTensor([0.0]).cuda()
-    criterion = nn.L1Loss()
     assert pred_map.size() == gt.size()
 
     if len(pred_map.size()) == 4:
@@ -37,7 +36,7 @@ def loss_func(pred_map, gt, args):
 
         loss /= pred_map.size(0)
         return loss
-
+    # print(f'loss_func {get_loss(pred_map, gt, args)} pred_map.size {pred_map.size(0)}')
     return get_loss(pred_map, gt, args)
 
 